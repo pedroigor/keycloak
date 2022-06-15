ec42f56c8f729b01f8c7abd4289b2a1abff9ee29
andreaTP <andrea.peruffo1982@gmail.com>
2022-06-15 09:34:58 +0100

    remove rebuild.sh

--- quarkus/rebuild.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-set -euxo pipefail
-
-(cd .. && mvn clean install -f config-api/pom.xml --offline)
-
-mvn clean package -DskipTests --offline
-
-tar -xvf dist/target/keycloak-999-SNAPSHOT.tar.gz -C dist/target/
